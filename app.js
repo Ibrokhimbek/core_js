22cb5a86b23639bcb1036fb98ba38e297f3909fb
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-05-05 00:43:07 +0500

    variables let const var

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-var name = 'Max'
-var age = 29
-var hasHobbies = true
+// var name = "Max";
+// let name = "Max";
+const name = "Max";
+// var age = 29;
+// let age = 29;
+const age = 29;
+// var hasHobbies = true;
+// let hasHobbies = true;
+const hasHobiies = true;
 
 function summarizeUser(userName, userAge, userHasHobbies) {
-    return (
-        'Name is ' +
-        userName +
-        ', age is ' +
-        userAge +
-        ' and the user has hobbies: ' +
-        userHasHobbies
-    )
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies: " +
+    userHasHobbies
+  );
 }
 
-console.log(summarizeUser(name, age, 'playing fotball'))
+console.log(summarizeUser(name, age, "playing fotball"));
