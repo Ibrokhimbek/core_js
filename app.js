a86f530b5f8c7118b3aeaff90a4b5a84caa47bc7
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-05-14 18:55:17 +0500

    Spread and Rest operators

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const person = {
   },
 };
 
+const copiedPerson = { ...person };
+console.log(copiedPerson);
+
 const hobbies = ["Sports", "Cooking"];
 // for (let hobby of hobbies) {
 //     console.log(hobby)
@@ -16,5 +19,12 @@ const hobbies = ["Sports", "Cooking"];
 // console.log(hobbies.map((hobby) => `Hobby: ${hobby}`));
 // console.log(hobbies);
 
-hobbies.push("Programming");
-console.log(hobbies);
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+//Rest operator
+const toArray = (...args) => {
+  return args;
+};
+
+console.log(toArray(1, 2, 3, 4));
