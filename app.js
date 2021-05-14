5d51218080faf05a43cb6f705b3a3158acbeeb0a
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-05-14 18:18:26 +0500

    arrow functions

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
-// var name = "Max";
-// let name = "Max";
 const name = "Max";
-// var age = 29;
-// let age = 29;
-const age = 29;
-// var hasHobbies = true;
-// let hasHobbies = true;
+let age = 29;
 const hasHobiies = true;
 
-// function summarizeUser(userName, userAge, userHasHobbies) {
-//   return (
-//     "Name is " +
-//     userName +
-//     ", age is " +
-//     userAge +
-//     " and the user has hobbies: " +
-//     userHasHobbies
-//   );
-// }
+age = 30;
 
 const summarizeUser = (userName, userAge, userHasHobbies) => {
-    return (
-        "Name is " +
+  return (
+    "Name is " +
     userName +
     ", age is " +
     userAge +
     " and the user has hobbies: " +
     userHasHobbies
-    )
-}
+  );
+};
+
+// const add = (a, b) => a + b;
+// const addOne = (a) => a + 1;
+const addRandom = () => 1 + 2;
+
+// console.log(add(1, 2));
+// console.log(addOne(3));
+console.log(addRandom());
 
 console.log(summarizeUser(name, age, "playing fotball"));
