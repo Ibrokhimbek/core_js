9cadf20755ddd30f5d2fa6a2c3fccf06e27be689
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-05-14 18:33:18 +0500

    push method of arrays

--- app.js
+++ app.js
@@ -13,5 +13,8 @@ const hobbies = ["Sports", "Cooking"];
 // for (let hobby of hobbies) {
 //     console.log(hobby)
 // }
-console.log(hobbies.map((hobby) => `Hobby: ${hobby}`));
+// console.log(hobbies.map((hobby) => `Hobby: ${hobby}`));
+// console.log(hobbies);
+
+hobbies.push("Programming");
 console.log(hobbies);
