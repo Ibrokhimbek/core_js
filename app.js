5ca237d4b6b27d4c9f0549462d33518987c6349b
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-05-14 18:24:14 +0500

    Objects in js

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-const name = "Max";
-let age = 29;
-const hasHobiies = true;
-
-age = 30;
-
-const summarizeUser = (userName, userAge, userHasHobbies) => {
-  return (
-    "Name is " +
-    userName +
-    ", age is " +
-    userAge +
-    " and the user has hobbies: " +
-    userHasHobbies
-  );
+const person = {
+  name: "Ibrokhim",
+  age: 20,
+  greet: () => {
+    console.log("Hi I'm " + person.name);
+  },
+  greet() {
+      console.log("Hi I'm " + person.name)
+  }
 };
 
-// const add = (a, b) => a + b;
-// const addOne = (a) => a + 1;
-const addRandom = () => 1 + 2;
-
-// console.log(add(1, 2));
-// console.log(addOne(3));
-console.log(addRandom());
-
-console.log(summarizeUser(name, age, "playing fotball"));
+person.greet();
