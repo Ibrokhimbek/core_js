cb436bec02ea709b2aa4134539475d32610f0d95
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-05-14 19:14:26 +0500

    Async code and Promise

--- app.js
+++ app.js
@@ -1,42 +1,24 @@
-const person = {
-  name: "Ibrokhim",
-  age: 20,
-  //   greet: () => {
-  //     console.log("Hi I'm " + person.name);
-  //   },
-  greet() {
-    console.log("Hi I'm " + this.name);
-  },
+// Async code and Promise
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, 1500);
+  });
+  return promise;
 };
 
-const printName = ({name, age}) => {
-    return console.log(name, age)
-}
+setTimeout(() => {
+  console.log("Timer is done!");
+  fetchData()
+    .then((text) => {
+      console.log(text);
+      return fetchData();
+    })
+    .then((text2) => {
+      console.log(text2);
+    });
+}, 2000);
 
-printName(person)
-
-
-const {name, age} = person
-console.log(name, age)
-
-// const copiedPerson = { ...person };
-// console.log(copiedPerson);
-
-const hobbies = ["Sports", "Cooking"];
-const [hobby1, hobby2] = hobbies
-console.log(hobby1, hobby2)
-// for (let hobby of hobbies) {
-//     console.log(hobby)
-// }
-// console.log(hobbies.map((hobby) => `Hobby: ${hobby}`));
-// console.log(hobbies);
-
-// const copiedArray = [...hobbies];
-// console.log(copiedArray);
-
-//Rest operator
-// const toArray = (...args) => {
-//   return args;
-// };
-
-// console.log(toArray(1, 2, 3, 4));
+console.log("Hello");
+console.log("Hi");
