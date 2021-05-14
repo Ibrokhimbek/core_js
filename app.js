8978776dd689311780b837a53b926f585c950228
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-05-14 19:03:59 +0500

    Destructuring objects and arrays

--- app.js
+++ app.js
@@ -9,22 +9,34 @@ const person = {
   },
 };
 
-const copiedPerson = { ...person };
-console.log(copiedPerson);
+const printName = ({name, age}) => {
+    return console.log(name, age)
+}
+
+printName(person)
+
+
+const {name, age} = person
+console.log(name, age)
+
+// const copiedPerson = { ...person };
+// console.log(copiedPerson);
 
 const hobbies = ["Sports", "Cooking"];
+const [hobby1, hobby2] = hobbies
+console.log(hobby1, hobby2)
 // for (let hobby of hobbies) {
 //     console.log(hobby)
 // }
 // console.log(hobbies.map((hobby) => `Hobby: ${hobby}`));
 // console.log(hobbies);
 
-const copiedArray = [...hobbies];
-console.log(copiedArray);
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
 
 //Rest operator
-const toArray = (...args) => {
-  return args;
-};
+// const toArray = (...args) => {
+//   return args;
+// };
 
-console.log(toArray(1, 2, 3, 4));
+// console.log(toArray(1, 2, 3, 4));
