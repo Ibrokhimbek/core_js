1059155861ab70b63aab248a913180c626a231e3
Ibrokhimbek <ibrokhim1348@gmail.com>
2021-05-14 18:29:35 +0500

    Arrays and array methods

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const person = {
   name: "Ibrokhim",
   age: 20,
-  greet: () => {
-    console.log("Hi I'm " + person.name);
-  },
+  //   greet: () => {
+  //     console.log("Hi I'm " + person.name);
+  //   },
   greet() {
-      console.log("Hi I'm " + person.name)
-  }
+    console.log("Hi I'm " + this.name);
+  },
 };
 
-person.greet();
+const hobbies = ["Sports", "Cooking"];
+// for (let hobby of hobbies) {
+//     console.log(hobby)
+// }
+console.log(hobbies.map((hobby) => `Hobby: ${hobby}`));
+console.log(hobbies);
